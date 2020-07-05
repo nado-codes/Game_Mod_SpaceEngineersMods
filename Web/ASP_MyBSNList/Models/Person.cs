528c5af891e99ab232ab3210de304e29add37252
nathan.linsley960731 <nathan.linsley960731@hotmail.com>
2020-07-06 06:51:20 +1000

    Initial commit of ASP_MyBSNList

---
+++ Web/ASP_MyBSNList/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using ASP_MyBSNList.Models.Components_Person;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MyBSNList.Models
+{
+    public class Person
+    {
+        [Required]
+        public Status Status { get; set; }
+
+        [Required]
+        public int StatusId { get; set; }
+
+        [Required]
+        public Reason Reason { get; set; }
+
+        [Required]
+        public int StatusId { get; set; }
+    }
+}
